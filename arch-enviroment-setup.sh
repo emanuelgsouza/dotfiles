2efa23bf85cea631a664ad8e070b5f56ba2ac221
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-06-27 18:17:01 -0300

    replace yaourt to yay

--- arch-enviroment-setup.sh
+++ arch-enviroment-setup.sh
@@ -1,36 +1,33 @@
-# https://www.ostechnix.com/install-yaourt-arch-linux/
-
 sudo pacman -S --needed base-devel git wget yajl --noconfirm && \
 
-yaourt -S sublime-text-dev smartgit telegram-desktop-bin --noconfirm && \
+yay -S smartgit telegram-desktop-bin --noconfirm && \
 
 ## nodejs ecosystem
-yaourt -S nodejs npm  yarn --noconfirm && \
+yay -S nodejs npm  yarn --noconfirm && \
 
 ## college
-yaourt -S astah-community --noconfirm && \
+yay -S astah-community --noconfirm && \
 
 ## IDEs
-# yaourt -S intellij-idea-ultimate-edition phpstorm goland && \
-yaourt -S eclipse-java && \
+yay -S phpstorm eclipse-java && \
 
 ## java environment
-yaourt -S java-environment=8 --noconfirm && \
-yaourt -S android-platform android-sdk android-sdk-platform-tools android-sdk-build-tools gradle --noconfirm && \
+yay -S java-environment=8 --noconfirm && \
+# yay -S android-platform android-sdk android-sdk-platform-tools android-sdk-build-tools gradle --noconfirm && \
 
 ## docker
-yaourt -S docker docker-compose --noconfirm && \
+yay -S docker docker-compose --noconfirm && \
 
 ## fonts
-yaourt -S ttf-fira-code powerline powerline-fonts ttf-liberation screenfetch figlet ttf-ubuntu-font-family --noconfirm && \
+yay -S ttf-fira-code powerline powerline-fonts ttf-liberation screenfetch figlet ttf-ubuntu-font-family --noconfirm && \
 
 ## utilities
-yaourt -S pgadmin3 dbeaver redis-desktop-manager insomnia --noconfirm && \
+yay -S pgadmin3 dbeaver redis-desktop-manager insomnia --noconfirm && \
 
-yaourt -S google-chrome atom terminator visual-studio-code-bin && \
+yay -S google-chrome atom terminator visual-studio-code-bin && \
 
 # enable gnome-extension
-yaourt -S gnome-tweaks chrome-gnome-shell && \
+yay -S gnome-tweaks chrome-gnome-shell && \
 
 # sudo groupadd docker && \
 sudo usermod -aG docker $USER && \
