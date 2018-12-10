7bd6604c9b20f6ff3f47a78900075cec96103b26
Emanuel Souza <emanuelgdesouza@gmail.com>
2018-12-09 22:36:55 -0200

    update env.fish

--- fish/env.fish
+++ fish/env.fish
@@ -1,4 +1,4 @@
-set -gx PATH ~/.npm-global/bin ~/.config/yarn/global/node_modules/.bin $PATH
+#set -gx PATH ~/.npm-global/bin ~/.config/yarn/global/node_modules/.bin $PATH
 # set -gx PATH /opt/android-sdk/platform-tools /opt/android-sdk/build-tools/26.0.1/ /opt/android-sdk/tools $PATH
 
 #set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk
