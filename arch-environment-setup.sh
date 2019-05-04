a6d194d70d6f5096a6c2bc7582857618ad55d606
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-05-04 15:05:32 -0300

    add ubuntu font

--- arch-environment-setup.sh
+++ arch-environment-setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+yay -S ttf-ubuntu-font-family && \
+
 # git
 git config --global alias.vlog 'log --graph --date-order --date=relative --pretty=format:"%C(white)%h: %Cgreen - %an - %Cred %C(cyan)%ar:%Creset%n%s%n" --color'
 
